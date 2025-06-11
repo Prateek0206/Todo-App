95e01b454399dc443525822731187f97423cf9df
Prateek <84000231+Prateek0206@users.noreply.github.com>
2025-06-12 01:39:24 +0530

    added the feature to close the nav bar by touching outside the nav area in smaller devices

--- scripts/nav-bar-animate.js
+++ scripts/nav-bar-animate.js
@@ -2,6 +2,7 @@ const hamburgerMenu = document.querySelector(".js-hamburger-menu");
 const closingMenu = document.querySelector(".js-close-button");
 const hamLinks = document.querySelector(".js-ham-links");
 const header = document.querySelector(".nav-bar");
+const mainContainer = document.querySelector(".js-main-content");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.add("hamburger-menu-inactive");
@@ -10,6 +11,13 @@ hamburgerMenu.addEventListener("click", () => {
   header.classList.add("blur-on");
 
   closingMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("hamburger-menu-inactive");
+    hamLinks.classList.remove("ham-links-active");
+    header.classList.remove("blur-on");
+    header.classList.add("blur-off");
+  });
+
+  mainContainer.addEventListener("click", () => {
     hamburgerMenu.classList.remove("hamburger-menu-inactive");
     hamLinks.classList.remove("ham-links-active");
     header.classList.remove("blur-on");
