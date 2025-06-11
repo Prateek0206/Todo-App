dbfb2572258b4b015bd0a6d88c7ec52a532e723d
Prateek <84000231+Prateek0206@users.noreply.github.com>
2025-06-11 22:58:11 +0530

    Created nav bar and made it responsive for smaller device using hamburger menu. Made an js file called hamburger-menu.js for interactivity

---
+++ scripts/nav-bar-animate.js
@@ -0,0 +1,16 @@
+const hamburgerMenu = document.querySelector(".js-hamburger-menu");
+const closingMenu = document.querySelector(".js-close-button");
+const hamLinks = document.querySelector(".js-ham-links");
+const header = document.querySelector('.nav-bar');
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.add("hamburger-menu-inactive");
+  hamLinks.classList.add("ham-links-active");
+  header.classList.add("blur");
+
+  closingMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("hamburger-menu-inactive");
+    hamLinks.classList.remove("ham-links-active");
+    header.classList.remove("blur");
+  });
+});
