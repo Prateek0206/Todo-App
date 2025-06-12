cc8dc481c8fb5b819ee626ea8ce01bf98c28345a
Prateek <84000231+Prateek0206@users.noreply.github.com>
2025-06-13 01:19:19 +0530

    added blur to social links cause I forgot to add

--- scripts/nav-bar-animate.js
+++ scripts/nav-bar-animate.js
@@ -3,17 +3,22 @@ const closingMenu = document.querySelector(".js-close-button");
 const hamLinks = document.querySelector(".js-ham-links");
 const header = document.querySelector(".nav-bar");
 const mainContainer = document.querySelector(".js-main-content");
+const socialLinks = document.querySelector(".js-social-links-responsive");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.add("hamburger-menu-inactive");
   hamLinks.classList.add("ham-links-active");
   header.classList.remove("blur-off");
   header.classList.add("blur-on");
+  socialLinks.classList.remove("blur-off");
+  socialLinks.classList.add("blur-on");
 
   closingMenu.addEventListener("click", () => {
     hamburgerMenu.classList.remove("hamburger-menu-inactive");
     hamLinks.classList.remove("ham-links-active");
     header.classList.remove("blur-on");
     header.classList.add("blur-off");
+    socialLinks.classList.remove("blur-on");
+    socialLinks.classList.add("blur-off");
   });
 });
